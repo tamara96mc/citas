d5d194b7d16c1d962634330354a01c20f55f77b2
tamara96mc <tamara.96mc@gmail.com>
2020-12-01 20:14:35 +0100

    Updates

---
+++ precache-manifest.88ad1cbae427ba5fad032b13810b8dcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f569b34e5cd508d349981474931704e0",
+    "url": "/citas/index.html"
+  },
+  {
+    "revision": "2ee9ab110a8af438d88d",
+    "url": "/citas/static/css/main.e7c542e3.chunk.css"
+  },
+  {
+    "revision": "4eeb61d24d793d114467",
+    "url": "/citas/static/js/2.e0ca3766.chunk.js"
+  },
+  {
+    "revision": "2ee9ab110a8af438d88d",
+    "url": "/citas/static/js/main.8bc5efe1.chunk.js"
+  },
+  {
+    "revision": "5124c4822506f662dcee",
+    "url": "/citas/static/js/runtime-main.5e42894a.js"
+  }
+]);
